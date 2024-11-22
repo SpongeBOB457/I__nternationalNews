c30352becc96f103bd6d4b98b3c4eaf27cea5e18
Adam fofo <adamsaddad700@gmail.com>
2024-11-22 19:00:06 +0100

    Add project files.

---
+++ I__nternationalNews/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace I__nternationalNews.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
